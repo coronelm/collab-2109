95728f004195fc1a837e3794f49dda7ecf8d548a
pixiepau <24-30246@g.batstate-u.edu.ph>
2025-11-07 07:37:54 +0800

    Joseph Villajuan updated column named head_teacher

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCRE
 CREATE TABLE departments (dept_id INT, dept_name VARCHAR(50));
 INSERT INTO departments (dept_id, dept_name) VALUES (1, 'Computer Science'), (2, 'Information Technology'), (3, 'Civil Engineering');
 ALTER TABLE departments ADD head_teacher VARCHAR(50);
+UPDATE departments SET head_teacher = 'Mr. Alimoren' WHERE dept_id = 2;
