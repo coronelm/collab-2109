19ff9a17b3a4963c4680dc7cb40afa0944a27d40
pixiepau <24-30246@g.batstate-u.edu.ph>
2025-11-07 07:14:31 +0800

    Jhon Paul Boa created a table named departments

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+CREATE TABLE departments (dept_id INT, dept_name VARCHAR(50));
