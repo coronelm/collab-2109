2414891a2bd539d3356fc73809395bdbf2839517
michaelandre-tanza <michaelandretanza887@gmail.com>
2025-11-06 20:20:57 +0800

    Tanza and Delos Santos created Teachers Table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,2 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+CREATE TABLE students(id INT, name VARCHAR(100), grade_level VARCHAR(50));
