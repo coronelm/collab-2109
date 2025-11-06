6eb38d25314e38600c98eabef06af327c299e384
Rxncx <rocreorancejherome@gmail.com>
2025-11-06 22:36:36 +0800

    Group9-Abrogina, Hillary: Inserted values to users table.

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 CREATE TABLE users (user_id INT, username VARCHAR(70), role VARCHAR(70));
 CREATE TABLE users (user_id INT, username VARCHAR(70), role VARCHAR(70));
+INSERT INTO users (username, role) VALUES ('admin_user', 'admin'), ('student_user', 'student');
