1ab5eefbfee094159e8dfe9512f1b1f323f34dd1
POnead <princedeano.rodriguez@gmail.com>
2025-11-06 21:27:47 +0800

    Group8-Jane: Added remarks column

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCRE
 CREATE TABLE Attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
 CREATE TABLE attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
 INSERT INTO attendance VALUES(1,1,11-06-25,Late)(2,2,11-07-25,Absent);
+ALTER TABLE attendance ADD remarks VARCHAR(255);
