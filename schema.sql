1fcd43a0755b07f9715135b200f155875d500968
michaelandre-tanza <michaelandretanza887@gmail.com>
2025-11-06 20:49:47 +0800

    Dimaculangan inserted three values inside Teacher's table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,6 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 CREATE TABLE students(id INT, name VARCHAR(100), grade_level VARCHAR(50));
+INSERT INTO teachers (id, name, subject) VALUES
+(01, 'Mr. Raymond', 'Physics'),
+(02, 'Ms. Marielle', 'Data Base Management'),
+(03, 'Ajarn. Chie', 'Advance Comprog');
