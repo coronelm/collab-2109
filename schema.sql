aac55f2c2ffb8e0e2177a37c289b3fd8297dd777
POnead <princedeano.rodriguez@gmail.com>
2025-11-06 18:35:54 +0800

    Prince Deano Rodriguez added table attendance.

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+CREATE TABLE Attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
+CREATE TABLE attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
