d1789eed117166c73f60d2529507625055fe33b5
TSUNANA2772 <ikingandrewjoseph@gmail.com>
2025-11-06 21:12:57 +0800

    Obedencio: INSERTED sample classes

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE TABLE classes (id INT, class_name VARCHAR(50), room_no VARCHAR(30));
 CREATE TABLE classes (id INT PRIMARY KEY, class_name VARCHAR(50), room_no VARCHAR(10));
+INSERT INTO classes (id, class_name, room_no) VALUES (1, 'Balba', 'RM305'), (2, 'Coronell', 'RM303');
