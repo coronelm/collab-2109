67edcec98d772812a1a438708ef25716ceb9abbd
POnead <princedeano.rodriguez@gmail.com>
2025-11-06 21:21:47 +0800

    Group8 Magsino, A J: Inserted 2 attendance records

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 CREATE TABLE Attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
 CREATE TABLE attendance (attendance_id INT, student_id INT, date VARCHAR(50), status VARCHAR(50));
+INSERT INTO attendance VALUES(1,1,11-06-25,Late)(2,2,11-07-25,Absent);
