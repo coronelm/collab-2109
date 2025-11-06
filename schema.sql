e43ec24ede89ae2bcf532d01ae86404b311d4766
TSUNANA2772 <ikingandrewjoseph@gmail.com>
2025-11-06 21:18:28 +0800

    Maquinto: ADDED teacher_id column

--- schema.sql
+++ schema.sql
@@ -1,3 +1,4 @@
 CREATE TABLE classes (id INT, class_name VARCHAR(50), room_no VARCHAR(30));
 CREATE TABLE classes (id INT PRIMARY KEY, class_name VARCHAR(50), room_no VARCHAR(10));
 INSERT INTO classes (id, class_name, room_no) VALUES (1, 'Balba', 'RM305'), (2, 'Coronell', 'RM303');
+ALTER TABLE classes ADD COLUMN teacher_id INT;
