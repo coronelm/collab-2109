92b5d7529ffda93c3b75e95868f084f9cde18564
pixiepau <24-30246@g.batstate-u.edu.ph>
2025-11-07 07:16:57 +0800

    Kayle Justin De Leon inserted 2-3 departments

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 CREATE TABLE departments (dept_id INT, dept_name VARCHAR(50));
+INSERT INTO departments (dept_id, dept_name) VALUES (1, 'Computer Science'), (2, 'Information Technology'), (3, 'Civil Engineering');
