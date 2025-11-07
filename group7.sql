4b9b7e2bd8b5ea5226d0cecc17a3dc426c2f7c8c
maegan14 <24-38345@g.batstate-u.edu.ph>
2025-11-07 11:13:16 +0800

    Karlo: Added units column to subjects table

--- group7.sql
+++ group7.sql
@@ -1,2 +1,3 @@
 CREATE TABLE subjects (subject_id INT PRIMARY KEY, subject_name VARCHAR(100));
 [200~INSERT INTO subjects VALUES (1, 'Mathematics'), (2, 'English'), (3, 'Science');
+ALTER TABLE subjects ADD units INT;
