26b634f24d6bb5ff137fa81656ffd23dcfa67c5b
Rxncx <rocreorancejherome@gmail.com>
2025-11-07 11:36:07 +0800

    Group9-Jimenez, Jamaica: Added column to users table.

--- schema.sql
+++ schema.sql
@@ -2,3 +2,4 @@ CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCRE
 CREATE TABLE users (user_id INT, username VARCHAR(70), role VARCHAR(70));
 CREATE TABLE users (user_id INT, username VARCHAR(70), role VARCHAR(70));
 INSERT INTO users (username, role) VALUES ('admin_user', 'admin'), ('student_user', 'student');
+ALTER TABLE users ADD COLUMN email VARCHAR(50);
