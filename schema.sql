daa4b36a16847f7990750d96f145b64bcaddae77
Ryan1017-p <24-33376@g.batstate-u.edu.ph>
2025-11-07 12:54:57 +0800

    Marco rafaeles created a new table

--- schema.sql
+++ schema.sql
@@ -1,2 +1,3 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
 CREATE TABLE students (students_id INT PRIMARY KEY, student_name VARCHAR(50), grade_level INT);
+INSERT INTO students VALUES(1, 'Marco Rafaeles', 2), (2, 'Christian Balita', 2), (3, 'Ryan Lacerna', 2);
