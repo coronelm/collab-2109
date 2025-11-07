15a9b0ee8111d14729b497e93ecc9b41ff820e35
v4short <ytrewqq270@gmail.com>
2025-11-07 15:26:22 +0800

    Tivera and Mejico created Teachers Table

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE DATABASE school_db;USE school_db;CREATE TABLE students (id INT AUTO_INCREMENT PRIMARY KEY, name VARCHAR(100), grade INT);
+CREATE TABLE students(id INT,name VARCHAR(30), grade_level VARCHAR(30)
+;
