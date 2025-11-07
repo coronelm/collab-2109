a783009c15e87653d28348dd7daa23176831c01e
Hannah Sarvia Doromal <hannahsarvia43@gmail.com>
2025-11-07 10:33:35 +0800

    Update enrollments.sql

--- enrollments.sql
+++ enrollments.sql
@@ -3,13 +3,3 @@ CREATE TABLE enrollments (
   student_id INT,
   class_id INT
 );
-CREATE TABLE enrollments (
-  enrollment_id INT PRIMARY KEY,
-  student_id INT,
-  class_id INT
-);
-CREATE TABLE enrollments (
-  enrollment_id INT PRIMARY KEY,
-  student_id INT,
-  class_id INT
-);
