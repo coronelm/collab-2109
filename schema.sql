a3591f0698ea57e2a8fc11744fd107fe56038268
v4short <ytrewqq270@gmail.com>
2025-11-07 15:47:23 +0800

    De Luna inserted three values inside Teacher's table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,8 @@ CREATE TABLE students(id INT,name VARCHAR(30), grade_level VARCHAR(30)
 ;
 CREATE TABLE students(id INT, name VARCHAR(40), grade_level VARCHAR(30)
 ; 
+INSERT INTO teachers (id, name, subject) VALUES
+(01, 'Mr. Raymond', 'Physics'),
+(02, 'Ms. Marielle', 'Data Base Management'),
+(03, 'Ajarn. Chie', 'Advance Comprog'); >> schema.sql
+;
